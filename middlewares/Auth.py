65814a3092d00840f93b0fdd11c5fc5a5caab924
Peter Damilare <mayokundamilare@gmail.com>
2023-03-22 19:17:48 +0200

    Updated Auth middleware

--- middlewares/Auth.py
+++ middlewares/Auth.py
@@ -8,16 +8,7 @@ class Auth(BaseHTTPMiddleware):
 		super().__init__()
 
 	def dispatch(self, request, call_next):
-		isAuthenticated = False
-		if request.path in public_routes:
-			return jsonify({'message': 'unauthenticated'})
-		elif request.path in ignored_routes:
-			return call_next(request)
-
 		if auth_check():
-			isAuthenticated = True
-
-		if isAuthenticated == True:
 			return call_next(request)
 		else:
 			return jsonify({'message': 'unauthenticated'})
